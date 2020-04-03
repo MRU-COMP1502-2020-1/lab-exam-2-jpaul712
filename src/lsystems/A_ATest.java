54a06ff7159aad4b921e392a9020f60742d5961a
Jashan Paul <jpaul712@mtroyal.ca>
2020-04-03 14:49:54 -0600

    Changes

--- src/lsystems/A_ATest.java
+++ src/lsystems/A_ATest.java
@@ -5,11 +5,22 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class A_ATest {
+	
+	private A_A a = new A_A();
 
 	@Test
-	public void test() 
+	public void testgetMatch() 
 	{
-		fail("Not yet implemented");
+		assertEquals("A", a.getMatch());
+		
 	}
+	
+	@Test
+	public void testgetBody() 
+	{
+		assertEquals("A", a.getBody());
+		
+	}
+
 
 }
--- src/lsystems/C_BTest.java
+++ src/lsystems/C_BTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class C_BTest {
 
 	@Test
-	public void testA_A() 
+	public void test() 
 	{
 		
 	}
